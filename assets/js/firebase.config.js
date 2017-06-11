ee666edc550ff41c4cd1ae1d106dcbeafc95953a
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2017-06-10 23:59:12 -0400

    added initial files and linked them all up

---
+++ assets/js/firebase.config.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = 
+{
+  apiKey: "AIzaSyD_tSPM0N384Ks2htt9j4aIinckYyE6wzE",
+  authDomain: "rps-multiplayer-e2292.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-e2292.firebaseio.com",
+  projectId: "rps-multiplayer-e2292",
+  storageBucket: "rps-multiplayer-e2292.appspot.com",
+  messagingSenderId: "978981637680"
+};
+
+firebase.initializeApp( config );
+
+console.log( "firebase initilized" );
